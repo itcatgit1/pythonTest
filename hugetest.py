9983d4f23319cb1999f5ffccae7480e57fb08ec7
itcatgit1 <1458210579@qq.com>
2018-10-08 17:29:07 +0800

    第一次提交代码

---
+++ hugetest.py
@@ -0,0 +1,23 @@
+print("hello word")
+A=10_23_54_432
+C=-100+43
+C=C+78787
+s = "子曰:\"学而时习之，\\\\n乐呵乐呵！\""
+print(s)
+print(C)
+print(A)
+bvfchs='胡歌'+'科比'+'陈焕超'
+print(bvfchs)
+b = 'hello %s 你好 %s'%('tom','孙悟空')
+print(b)
+huge='123456789'
+huge1=123456789
+print(type(huge))
+print(type(huge1))
+
+
+
+
+
+
+
